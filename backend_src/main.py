2332371e45ca9b375a0162713fa4a81152e1b03c
Keyhole-Koro <korokororinthird@gmail.com>
2025-05-17 20:37:55 +0900

    add linters flake8

--- backend_src/main.py
+++ backend_src/main.py
@@ -8,5 +8,11 @@ load_dotenv()
 groq_api_key = os.getenv('GROQ_API_KEY')
 
 if __name__ == "__main__":
-    result = ask_groq("Explain the importance of fast language models", apikey=groq_api_key)
+    if groq_api_key is None:
+        raise ValueError("GROQ_API_KEY is not set in the environment variables.")
+    
+    result = ask_groq(
+        "Explain the importance of fast language models", 
+        apikey=groq_api_key
+    )
     print("result: ", result)
